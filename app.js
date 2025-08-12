225cd0d523efc90f3b2c17f7048d4a0d899ea43b
Tiago Guimarães <tab.guimaraes@gmail.com>
2025-08-12 10:45:04 -0300

    codigo finalizado

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-import funcionarios from "./funcionarios.json" with {type: "json"}
+import funcionarios from "./funcionarios.json" with {type: "json"};
 
-console.log(funcionarios[2].nome)
+funcionarios.forEach((funcionario) => {
+    const body = document.querySelector('body');
+
+    const div = document.createElement('div');
+    div.classList.add("card");
+
+    const picture = document.createElement('picture');
+    picture.style.backgroundImage = `url("./img/${funcionario.imagem}")`; 
+    div.appendChild(picture);
+
+    const p = document.createElement('p');
+    p.innerText = funcionario.nome;
+    div.appendChild(p);
+
+    const span = document.createElement('span');
+    span.innerText = funcionario.cargo;
+    div.appendChild(span);
+
+    body.appendChild(div);
+});
